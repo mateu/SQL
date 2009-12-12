af33fc9f91f698f3b9e863629b0569f09b365aa6
mateu x hunter <hunter@missoula.org>
2009-12-11 20:58:19 -0500

    Assignment 6 finalized

---
+++ drop-tables.sql
@@ -0,0 +1,11 @@
+drop table if exists mxhunter_answers_given;
+drop table if exists mxhunter_questions2admins;
+drop table if exists mxhunter_students2classes;
+drop table if exists mxhunter_classes;
+
+drop table if exists mxhunter_admins;
+drop table if exists mxhunter_answers;
+drop table if exists mxhunter_questions;
+drop table if exists mxhunter_schools;
+drop table if exists mxhunter_teachers;
+drop table if exists mxhunter_students;
--- mxhunter_a6.sql
+++ mxhunter_a6.sql
@@ -7,7 +7,21 @@
 # names will be defined as type TEXT
 # strings of known maximum length will be defined as VARCHAR(max_length).
 # integer id fields of the entity tables are set to type INTEGER with 
-# AUTO_INCREMENT.
+# AUTO_INCREMENT.  This is a straight conversion of the ER model including
+# foreign keys.
+
+# Drop tables if they already exists
+drop table if exists mxhunter_answers_given;
+drop table if exists mxhunter_questions2admins;
+drop table if exists mxhunter_students2classes;
+drop table if exists mxhunter_classes;
+
+drop table if exists mxhunter_admins;
+drop table if exists mxhunter_answers;
+drop table if exists mxhunter_questions;
+drop table if exists mxhunter_schools;
+drop table if exists mxhunter_teachers;
+drop table if exists mxhunter_students;
 
 
 # -- Define Entity Tables --#
@@ -17,6 +31,7 @@ create table mxhunter_schools (
 	name text NOT NULL,
 	PRIMARY KEY(id)
 )
+;
 
 # teachers
 create table mxhunter_teachers (
@@ -24,6 +39,7 @@ create table mxhunter_teachers (
 	name text NOT NULL,
 	PRIMARY KEY(id)
 )
+;
 
 # students
 create table mxhunter_students (
@@ -31,6 +47,7 @@ create table mxhunter_students (
 	name text NOT NULL,
 	PRIMARY KEY(id)
 )
+;
 
 # admins (tests administered)
 create table mxhunter_admins (
@@ -40,20 +57,24 @@ create table mxhunter_admins (
 	grade integer NOT NULL,
 	PRIMARY KEY(id)
 )
+;
 
 # questions on the test (admin)
 create table mxhunter_questions (
 	id integer NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(id)
 )
+;
 
 # answers for questions
 create table mxhunter_answers (
 	id          integer NOT NULL AUTO_INCREMENT,
 	question_id integer NOT NULL,
 	score integer NOT NULL,
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
+	CONSTRAINT answers_FK_question FOREIGN KEY(question_id) REFERENCES mxhunter_questions(id)
 )
+;
 
 # -- Define Relational Tables - All have composite keys, i.e. multi-column #
 # classes within a school
@@ -62,10 +83,11 @@ create table mxhunter_classes (
 	teacher_id integer NOT NULL,
 	admin_id   integer NOT NULL,
 	PRIMARY KEY(school_id, teacher_id, admin_id),
-	CONSTRAINT classes_FK_school FOREIGN KEY(school_id) REFERENCES schools(id),
-	CONSTRAINT classes_FK_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
-	CONSTRAINT classes_FK_admin FOREIGN KEY(admin_id) REFERENCES admin(id)
+	CONSTRAINT classes_FK_school  FOREIGN KEY(school_id)  REFERENCES mxhunter_schools(id),
+	CONSTRAINT classes_FK_teacher FOREIGN KEY(teacher_id) REFERENCES mxhunter_teachers(id),
+	CONSTRAINT classes_FK_admin   FOREIGN KEY(admin_id)   REFERENCES mxhunter_admins(id)
 )
+;
 
 # students assigned to classes
 create table mxhunter_students2classes (
@@ -74,20 +96,22 @@ create table mxhunter_students2classes (
 	school_id  integer NOT NULL,
 	admin_id   integer NOT NULL,
 	PRIMARY KEY(student_id, teacher_id, school_id, admin_id),
-	CONSTRAINT classes_FK_student FOREIGN KEY(student_id) REFERENCES students(id),
-	CONSTRAINT classes_FK_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
-	CONSTRAINT classes_FK_school  FOREIGN KEY(school_id)  REFERENCES schools(id),
-	CONSTRAINT classes_FK_admin   FOREIGN KEY(admin_id)   REFERENCES admin(id)
+	CONSTRAINT classes_FK_student FOREIGN KEY(student_id) REFERENCES mxhunter_students(id),
+	CONSTRAINT classes_FK_teacher FOREIGN KEY(teacher_id) REFERENCES mxhunter_teachers(id),
+	CONSTRAINT classes_FK_school  FOREIGN KEY(school_id)  REFERENCES mxhunter_schools(id),
+	CONSTRAINT classes_FK_admin   FOREIGN KEY(admin_id)   REFERENCES mxhunter_admins(id)
 )
+;
 
 # questions that make up an admin
-create table mxhunter_questions2admin (
+create table mxhunter_questions2admins (
 	question_id integer NOT NULL,
-	admin_id integer NOT NULL,
+	admin_id    integer NOT NULL,
 	PRIMARY KEY(question_id, admin_id),
-	CONSTRAINT questions2admin_FK_question FOREIGN KEY(question_id) REFERENCES questions(id),
-	CONSTRAINT questions2admin_FK_admin    FOREIGN KEY(admin_id)    REFERENCES admins(id)
+	CONSTRAINT questions2admins_FK_question FOREIGN KEY(question_id) REFERENCES mxhunter_questions(id),
+	CONSTRAINT questions2admins_FK_admin    FOREIGN KEY(admin_id)    REFERENCES mxhunter_admins(id)
 )
+;
 
 # answers given by a student for an admin
 create table mxhunter_answers_given  (
@@ -96,9 +120,9 @@ create table mxhunter_answers_given  (
 	question_id integer NOT NULL,
 	answer_id   integer NOT NULL,
 	PRIMARY KEY(student_id, admin_id, question_id, answer_id),
-	CONSTRAINT answers_given_FK_student  FOREIGN KEY(student_id)  REFERENCES students(id),
-	CONSTRAINT answers_given_FK_admin    FOREIGN KEY(admin_id)    REFERENCES admins(id),
-	CONSTRAINT answers_given_FK_question FOREIGN KEY(question_id) REFERENCES questions(id),
-	CONSTRAINT answers_given_FK_answer   FOREIGN KEY(answer_id)   REFERENCES answers(id)
+	CONSTRAINT answers_given_FK_student  FOREIGN KEY(student_id)  REFERENCES mxhunter_students(id),
+	CONSTRAINT answers_given_FK_admin    FOREIGN KEY(admin_id)    REFERENCES mxhunter_admins(id),
+	CONSTRAINT answers_given_FK_question FOREIGN KEY(question_id) REFERENCES mxhunter_questions(id),
+	CONSTRAINT answers_given_FK_answer   FOREIGN KEY(answer_id)   REFERENCES mxhunter_answers(id)
 )
-
+;
